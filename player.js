9ab53144000add20b82dbe63eb24e58e9c4333c7
aamir <aamir@LAPTOP-4UP166TP>
2024-11-02 12:15:33 -0400

    pushing my additions. game does not work atm but i will continue working on it

--- player.js
+++ player.js
@@ -1,7 +1,35 @@
+var centerX = 1250 / 4, centerY = 850 / 4;
 export class Player {
+
+    preload(){
+        this.scene.load.spritesheet('player_idle', 'assets/spritesheets/hero_idlesheet.png', { frameWidth: 44, frameHeight: 48 });
+        this.preload.spritesheet('player_walk', 'assets/spritesheets/hero_walkingsheet.png', 44 , 48);
+    }
+    
+    create(){
+        console.log('player creation0');
+        player = this.add.sprite(centerX, centerY, 'player_idle')
+        this.scene.anims.create({
+            key: 'idle',
+            frames: this.scene.anims.generateFrameNumbers('player_idle', { start: 0, end: 3 }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.scene.anims.create({
+            key: 'walk',
+            frames: this.scene.anims.generateFrameNumbers('player_walk', { start: 0, end: 5 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.sprite.anims.play('idle');
+
+
+    }
     constructor(scene) {
         this.scene = scene;
-        this.sprite = this.scene.physics.add.sprite(100, 450, 'player').setScale(1);
+        this.sprite = this.scene.physics.add.sprite(100, 450, 'player_idle').setScale(1);
         this.sprite.setBounce(0.2);
         this.sprite.setCollideWorldBounds(true);
 
@@ -18,10 +46,15 @@ export class Player {
         // Player movement controls
         if (this.cursors.left.isDown) {
             this.sprite.setVelocityX(-160);
+            this.sprite.setScale(-1,1)
         } else if (this.cursors.right.isDown) {
             this.sprite.setVelocityX(160);
+            this.sprite.setScale(1,1)
+
+            //this.animations.play('walk', 14, true);
         } else {
             this.sprite.setVelocityX(0);
+            //this.animations.play('idle', 14, true);
         }
 
         // Jumping mechanics
