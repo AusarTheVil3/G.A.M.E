1ba153ec8a818747f14d6d09e47a655bd7a7f0a2
jcopham <jcopham117@gmail.com>
2024-11-20 15:04:01 -0500

    Addresses #1, adds modularlity by adding in scenes

---
+++ gameScene.js
@@ -0,0 +1,42 @@
+import { Player } from './player.js';
+import { Level } from './level.js';
+import { Enemy } from './enemy.js';
+
+class GameScene extends Phaser.Scene {
+    constructor(){
+        super('gameScene')
+    }
+
+    preload() {
+        this.load.image('player', 'https://opengameart.org/sites/default/files/styles/medium/public/asset/cyborg.png');
+        this.load.image('enemy', 'https://opengameart.org/sites/default/files/styles/medium/public/asset/lol.png');
+        this.load.image('ground', 'https://opengameart.org/sites/default/files/styles/medium/public/asset/platform.png');
+        this.load.image('base', 'https://opengameart.org/sites/default/files/styles/medium/public/asset/base.png');
+    }
+    
+    create() {
+        this.keyboardp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+        this.level = new Level(this);
+        this.player = new Player(this);
+        this.enemy = new Enemy(this);
+    
+        // Colliders
+        this.physics.add.collider(player.sprite, level.ground);
+        this.physics.add.collider(player.sprite, level.platforms);
+        this.physics.add.collider(enemy.sprite, level.ground);
+        this.physics.add.collider(enemy.sprite, level.platforms);
+    }
+    
+    update() {
+        
+        if (Phaser.Input.Keyboard.JustDown(this.keyboardp)) {
+            this.scene.switch('titleScene');
+        }
+
+        this.player.update();
+        this.enemy.update();
+    }
+
+}
+
+export default GameScene;
